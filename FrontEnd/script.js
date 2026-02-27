3c78253c4784ad1a42bf9544433543de315cdad8
Olivier Ba <olivier.barontini@gmail.com>
2026-02-27 10:14:45 +0100

    ajout de fichier d'enregitrement de session

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,2 +1,26 @@
-let salut = "ok";
-console.log(salut);
+fetch("http://localhost:5678/api/works")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log("Données reçues :", data);
+
+    const gallery = document.querySelector(".gallery");
+
+    data.forEach((work) => {
+      const figure = document.createElement("figure");
+
+      const img = document.createElement("img");
+      img.src = work.imageUrl;
+      img.alt = work.title;
+
+      const figcaption = document.createElement("figcaption");
+      figcaption.textContent = work.title;
+
+      figure.appendChild(img);
+      figure.appendChild(figcaption);
+
+      gallery.appendChild(figure);
+    });
+  })
+  .catch((error) => {
+    console.error("Erreur :", error);
+  });
